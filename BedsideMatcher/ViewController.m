f26d8c966449c30d6b641f25fcee3b3820183b35
Fresh Prince <hirsp4@bfh.ch>
2015-04-16 13:08:38 +0200

    CMA deleted

--- BedsideMatcher/ViewController.m
+++ BedsideMatcher/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "CMASupplyChainServicePortBinding.h"
-#import "CMAwebServiceResult.h"
-#import "CMAitem.h"
+#import "SupplyChainServicePortBinding.h"
+#import "webServiceResult.h"
+#import "item.h"
 #import "BeaconViewController.h"
 @interface ViewController ()
 
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    CMASupplyChainServicePortBinding* service = [[CMASupplyChainServicePortBinding alloc]init];
+    SupplyChainServicePortBinding* service = [[SupplyChainServicePortBinding alloc]init];
     NSLog(@"%@",[service sayHelloWorldFrom:@"Patrizia" __error:nil]);
     }
 
